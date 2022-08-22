1e4488fa0df4ca1a7b2c6b76adfed25c10107d9e
USERSAD\msergios <mario.sergiosilva@everis.nttdata.com>
2022-08-22 11:30:29 +0100

    MSG-P02: QUERIES CUSTOMIZADAS

--- src/main/java/demo/model/SalesAnalitics.java
+++ src/main/java/demo/model/SalesAnalitics.java
@@ -3,11 +3,39 @@ package demo.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public interface SalesAnalitics  {   
+public class SalesAnalitics  {   
 
-	public String getAggregate();
-		
-	public double getAmount();
+	
+	//@JsonProperty("Aggregate")
+		private String aggregate;
+	 
+		//@JsonProperty("Sales Amount")
+		private double amount;
+
+		//@JsonProperty("Aggregate")
+		public String getAggregate() {
+			return aggregate;
+		}
+
+		public void setAggregate(String aggregate) {
+			this.aggregate = aggregate;
+		}
+
+		//@JsonProperty("Sales Amount")
+		public double getAmount() {
+			return amount;
+		}
+
+		public void setAmount(double amount) {
+			this.amount = amount;
+		}
+
+
+		public SalesAnalitics(String aggregate, double amount) {
+			super();
+			this.aggregate = aggregate;
+			this.amount = amount;
+		}
 	
 	
 
--- src/main/java/demo/repository/order/OrderRepository.java
+++ src/main/java/demo/repository/order/OrderRepository.java
@@ -22,13 +22,30 @@ public interface OrderRepository extends JpaRepository<OrderEntity, Long>, Order
 	public List<OrderEntity> findByIdOrderByTotalValueAsc(Long id);
 	
 	
-
-	// COM JSQL houve erro para identificar items.product
+	
+	
+	// NOTA: NÃO FUNCIONOU COM JPQL E A CLASSE SalesAnalitics
+	//@Query("Select new demo.model.SalesAnalitics(product.name, SUM(totalValue)) from OrderItemEntity  GROUP BY product.name")
+	
+	
+	// NOTA: COM JSQL houve erro para identificar items.product 
 	//@Query("Select items.product.name, SUM(totalValue) from Order group by items.product.name")
 	
-	//new demo.model.SalesAnalitics(customer.name, SUM(totalValue))
+	
+	
+	/* NOTA: FUNCIONA, MAS COM RETORNO List<Object>
+	 * Se tentar usar a classe SalesAnalitics ocorre erro na mapeamento: 
+	 * No converter found capable of converting from type [org.springframework.data.jpa.repository.query.AbstractJpaQuery$TupleConverter$TupleBackedMap] to type [demo.model.SalesAnalitics]
+	*/
+	//@Query(nativeQuery = true, value = "select c.name as aggregate, sum(a.total_value) as amount from TB_ORDER a join TB_ORDER_ITEMS b on (b.order_id = a.id) join TB_PRODUCT c on (c.code = b.product_code) group by c.name")	
+	//public List<Object> aggregatedSalesByProduct2();
+	
+	
+	/* NOTA: com quey nativa, só funciona se o retorno for uma interface (neste caso ISalesAnalitics).
+	 */
 	@Query(nativeQuery = true, value = "select c.name as aggregate, sum(a.total_value) as amount from TB_ORDER a join TB_ORDER_ITEMS b on (b.order_id = a.id) join TB_PRODUCT c on (c.code = b.product_code) group by c.name")
 	public List<ISalesAnalitics> aggregatedSalesByProduct();
+	
 	
 	//public Double aggregatedSalesByProduct(Date startDate, Date endDate);
 
