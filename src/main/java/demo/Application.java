f9523d204ba9a87985596873b626926725b31852
USERSAD\msergios <mario.sergiosilva@everis.nttdata.com>
2022-08-26 12:33:00 +0100

    MSG-P03: conection to EUREKA

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -2,10 +2,14 @@ package demo;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableAutoConfiguration 
 public class Application {
 
     public static void main(String[] args) {
