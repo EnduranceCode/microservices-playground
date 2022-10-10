e01f323f26555db672e3f04d5b080be7d9842b32
Ricardo do Canto <rferrcan@emeal.nttdata.com>
2022-10-10 14:57:41 +0100

    Spring initializr

---
+++ src/main/java/com/nttdata/di4/netflixsubscriptions/NetflixSubscriptionsApplication.java
@@ -0,0 +1,12 @@
+package com.nttdata.di4.netflixsubscriptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixSubscriptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixSubscriptionsApplication.class, args);
+    }
+}
