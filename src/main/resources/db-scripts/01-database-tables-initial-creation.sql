371e73ff34234083bb8bcfca3e820859a670f0ca
Ricardo do Canto <rferrcan@emeal.nttdata.com>
2022-10-11 08:27:28 +0100

    Fine tune the database creation script

--- src/main/resources/db-scripts/01-database-tables-initial-creation.sql
+++ src/main/resources/db-scripts/01-database-tables-initial-creation.sql
@@ -14,7 +14,7 @@ CREATE USER 'netflix-user'@'localhost' IDENTIFIED WITH caching_sha2_password BY 
 GRANT ALL PRIVILEGES ON netflix_subscriptions.* TO 'netflix-user'@'localhost';
 
 CREATE TABLE IF NOT EXISTS netflix_subscriptions.subscription (
-  id BIGINT NOT NULL AUTO_INCREMENT,
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
   type VARCHAR(255) NOT NULL,
   price DECIMAL NOT NULL,
   start_date DATE NOT NULL,
@@ -24,11 +24,11 @@ CREATE TABLE IF NOT EXISTS netflix_subscriptions.subscription (
 );
 
 CREATE TABLE IF NOT EXISTS netflix_subscriptions.profile (
-  id BIGINT NOT NULL AUTO_INCREMENT,
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   alias VARCHAR(255) NOT NULL,
-  avatar LONGTEXT NULL,
-  subscription_id BIGINT NOT NULL,
+  avatar VARCHAR(1020) NULL,
+  subscription_id BIGINT(20) NOT NULL,
   CONSTRAINT pk_profile PRIMARY KEY (id),
   UNIQUE INDEX UNIQUE_ID (id ASC) VISIBLE,
   CONSTRAINT FK_PROFILE_SUBSCRIPTION_ID FOREIGN KEY (subscription_id) REFERENCES subscription (id)
