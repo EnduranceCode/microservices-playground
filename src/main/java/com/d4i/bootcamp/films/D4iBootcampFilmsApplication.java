7573e6e22af5e27f959051453c708dd80628d325
Ricardo do Canto <rferrcan@emeal.nttdata.com>
2022-10-12 16:59:32 +0100

    Spring initializr

---
+++ src/main/java/com/d4i/bootcamp/films/D4iBootcampFilmsApplication.java
@@ -0,0 +1,13 @@
+package com.d4i.bootcamp.films;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D4iBootcampFilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D4iBootcampFilmsApplication.class, args);
+	}
+
+}
