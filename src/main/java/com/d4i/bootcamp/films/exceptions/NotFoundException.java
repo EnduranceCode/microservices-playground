25d6af0a7a580b68d787fdc27d207f1a6906919e
Ricardo do Canto <rferrcan@emeal.nttdata.com>
2022-10-13 11:27:28 +0100

    Add Exceptions system

---
+++ src/main/java/com/d4i/bootcamp/films/exceptions/NotFoundException.java
@@ -0,0 +1,18 @@
+package com.d4i.bootcamp.films.exceptions;
+
+import com.d4i.bootcamp.films.dto.ErrorDto;
+import java.util.Collections;
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends D4iBootcampException {
+
+    private static final long serialVersionUID = -5771220719001568396L;
+
+    public NotFoundException(final String message) {
+        super(HttpStatus.NOT_FOUND.value(), message);
+    }
+
+    public NotFoundException(final String message, final ErrorDto data) {
+        super(HttpStatus.NOT_FOUND.value(), message, Collections.singletonList(data));
+    }
+}
---
+++ src/main/java/com/d4i/bootcamp/films/utils/constants/ExceptionConstants.java
@@ -0,0 +1,10 @@
+package com.d4i.bootcamp.films.utils.constants;
+
+public class ExceptionConstants {
+
+    public static final String ERROR = "ERROR";
+
+    private ExceptionConstants() {
+        throw new IllegalStateException("Utility Class");
+    }
+}
