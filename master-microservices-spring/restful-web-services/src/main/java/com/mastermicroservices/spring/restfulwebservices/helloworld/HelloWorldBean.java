af8e1fa984fa48422902cacdcca9456e90e2c482
Ricardo do Canto <endurancecode@gmail.com>
2022-10-24 17:12:59 +0100

    MMS-S03C16: Add the endpoint /hello-world-bean

---
+++ master-microservices-spring/restful-web-services/src/main/java/com/mastermicroservices/spring/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,23 @@
+package com.mastermicroservices.spring.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" + "message='" + message + '\'' + '}';
+    }
+}
