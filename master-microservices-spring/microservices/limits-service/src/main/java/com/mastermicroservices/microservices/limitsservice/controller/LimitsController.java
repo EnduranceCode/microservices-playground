4a1eacda2b3f19c50daad664b60ad3c48cc012c7
Ricardo do Canto <endurancecode@gmail.com>
2022-10-25 14:35:59 +0100

    MMS-S06C127: Get Limits values from application properties file

--- master-microservices-spring/microservices/limits-service/src/main/java/com/mastermicroservices/microservices/limitsservice/controller/LimitsController.java
+++ master-microservices-spring/microservices/limits-service/src/main/java/com/mastermicroservices/microservices/limitsservice/controller/LimitsController.java
@@ -1,15 +1,24 @@
 package com.mastermicroservices.microservices.limitsservice.controller;
 
 import com.mastermicroservices.microservices.limitsservice.bean.Limits;
+import com.mastermicroservices.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    private final Configuration configuration;
+
+    @Autowired
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping(path = "/limits")
     public Limits retrieveLimits() {
 
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
